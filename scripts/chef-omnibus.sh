17d727913efd49adfae70991c905447be71b4910
Flowers, Jay <jay.flowers@agilex.com>
2014-07-02 08:52:57 -0400

    ...

--- scripts/chef-omnibus.sh
+++ scripts/chef-omnibus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # http://www.opscode.com/chef/install
-curl -L https://www.opscode.com/chef/install.sh | bash
+curl -L https://nexus.vistacore.us/repositories/filerepo/com/opscode/osx-chef-client/10.14.4/osx-chef-client-10.14.4.sh | bash
 
 # Force exiting zero because the installer doesn't yet recognize 10.9 as
 # a valid install target
